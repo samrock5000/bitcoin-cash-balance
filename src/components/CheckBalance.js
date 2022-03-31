ef0d5a427139b730883ea429b7e16c644812bcb5
Samuel Nevarez <35318078+samrock5000@users.noreply.github.com>
2022-03-31 00:09:49 -0500

    Update CheckBalance.js

--- src/components/CheckBalance.js
+++ src/components/CheckBalance.js
@@ -17,7 +17,7 @@ const CheckBalance =(props) => {
 // REST API servers.
 const BCHN_MAINNET = 'https://bchn.fullstack.cash/v5/'
 // const ABC_MAINNET = 'https://abc.fullstack.cash/v4/'
-// const TESTNET3 = 'https://testnet3.fullstack.cash/v4/'
+const TESTNET3 = 'https://testnet3.fullstack.cash/v4/'
 
 // bch-js-examples require code from the main bch-js repo
 const BCHJS = require('@psf/bch-js')
